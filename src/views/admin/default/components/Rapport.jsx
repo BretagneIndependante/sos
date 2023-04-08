f2bdbaa9afe95c017928054964be037772705e26
DinoMalin <114430228+DinoMalin@users.noreply.github.com>
2023-04-08 02:38:07 +0200

    Rapport fini

--- src/views/admin/default/components/Rapport.jsx
+++ src/views/admin/default/components/Rapport.jsx
@@ -19,9 +19,8 @@ const Rapport = () => {
         
       <Slider image={image.slice(0,5)}/>
 
-      <div className="md:mt-16 lg:mt-0">
-        <div className="h-[250px] w-full xl:h-[350px]">
-        </div>
+      <div className="md:mt-16 lg:mt-0 p-2">
+        <textarea className='rounded-[15px] resize-none bg-navy-50 w-full p-4 shadow' placeholder='Ecrivez votre rapport...' name="text" id="" cols="30" rows="10"></textarea>
       </div>
   </Card>
   );
